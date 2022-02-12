7cade18785ed0e9484a57be2f9d3d2c17665e9cf
Mark Raudenbush <markraud@hotmail.com>
2022-02-12 06:03:10 -0700

    completed add and subtract sections of flashcard app

--- flash.py
+++ flash.py
@@ -9,9 +9,86 @@ root.title('Flashcard App!')
 root.geometry('400x400')
 root.iconbitmap('C:\\marks_work_docs\\development\\tkinter_intro\\myicon.ico')  
 
+
+# create function to determine subtract answer correct?
+def subtractCorrect(num_1, num_2):   
+    correct = num_1 - num_2
+    
+    # Correct and Incorrect message
+    outputAnswerCorrect = StringVar()
+    outputAnswerIncorrect = StringVar()
+    outputAnswerCorrect.set('Correct ' + str(num_1) + ' - ' + str(num_2) + ' = ' + str(correct))
+    outputAnswerIncorrect.set('Incorrect ' + str(num_1) + ' - ' + str(num_2) + ' = ' + str(correct) + ', Not ' + subtractAnswer.get())
+
+    if int(subtractAnswer.get()) == correct:
+        subtractCorrectLabel.config(text=outputAnswerCorrect.get())
+    else:
+        subtractCorrectLabel.config(text=outputAnswerIncorrect.get())
+
+    # Clear the answer box(subtractAnswer)
+    subtractAnswer.delete(0, 'end')  # the 0 is the first index and it holds whatever you typed in the Entry box
+
+    # Generate two new random numbers(these are IntVars so we should just need to set them)
+    num1.set(randint(0,10))
+    num2.set(randint(0,10))
+    subtractFlash.config(text=str(num1.get()) + ' - ' + str(num2.get()), font=('helvetica', 72) )
+
+# Create subtract function
+def subtract():
+    hideMenuFrames()
+    subtractFrame.pack(fill='both', expand=1)
+
+    # create 2 random integers between 0 and 10
+    global num1
+    global num2
+    num1 = IntVar()
+    num2 = IntVar()
+    num1.set(randint(0,10))
+    num2.set(randint(0,10))
+
+    # Put random integers on the screen
+    global subtractFlash
+    subtractFlash = Label(subtractFrame, text=str(num1.get()) + ' - ' + str(num2.get()), font=('helvetica', 72))
+    subtractFlash.pack(pady=10)
+
+    #Answer box for subtract function
+    global subtractAnswer
+    subtractAnswer = Entry(subtractFrame)
+    subtractAnswer.pack(pady=5)
+
+    #Answer button
+    subtractButton = Button(subtractFrame, text='Answer', command=lambda: subtractCorrect(num1.get(),num2.get()))
+    subtractButton.pack(pady=5)
+
+    # Correct or Incorrect Message
+    global subtractCorrectLabel
+    subtractCorrectLabel = Label(subtractFrame, text='Enter Your Answer Above')
+    subtractCorrectLabel.pack(pady=5)
+
+
 # create function to determine add answer correct?
-def addCorrect(num_1, num_2):
-    pass
+def addCorrect(num_1, num_2):   
+    # Calculate the actual answer.  these num_1/num_2 get passed in from the addButton(usin underscores cause they are different)
+    correct = num_1 + num_2
+    
+    # Correct and Incorrect message
+    outputAnswerCorrect = StringVar()
+    outputAnswerIncorrect = StringVar()
+    outputAnswerCorrect.set('Correct ' + str(num_1) + ' + ' + str(num_2) + ' = ' + str(correct))
+    outputAnswerIncorrect.set('Incorrect ' + str(num_1) + ' + ' + str(num_2) + ' = ' + str(correct) + ', Not ' + addAnswer.get())
+
+    if int(addAnswer.get()) == correct:
+        addCorrectLabel.config(text=outputAnswerCorrect.get())
+    else:
+        addCorrectLabel.config(text=outputAnswerIncorrect.get())
+
+    # Clear the answer box(addAnswer)
+    addAnswer.delete(0, 'end')  # the 0 is the first index and it holds whatever you typed in the Entry box
+
+    # Generate two new random numbers(these are IntVars so we should just need to set them)
+    num1.set(randint(0,10))
+    num2.set(randint(0,10))
+    addFlash.config(text=str(num1.get()) + ' + ' + str(num2.get()), font=('helvetica', 72) )
 
 # Create add function
 def add():
@@ -27,10 +104,12 @@ def add():
     num2.set(randint(0,10))
 
     # Put random integers on the screen
-    addFlash = Label(addFrame, text=str(num1.get()) + ' + ' + str(num2.get()), font=('helvetica', 32) )
+    global addFlash
+    addFlash = Label(addFrame, text=str(num1.get()) + ' + ' + str(num2.get()), font=('helvetica', 72) )
     addFlash.pack(pady=10)
 
     #Answer box for add function
+    global addAnswer
     addAnswer = Entry(addFrame)
     addAnswer.pack(pady=5)
 
@@ -38,10 +117,15 @@ def add():
     addButton = Button(addFrame, text='Answer', command=lambda: addCorrect(num1.get(),num2.get()))
     addButton.pack(pady=5)
 
-# Create subtract function
-def subtract():
-    hideMenuFrames()
-    subtractFrame.pack(fill='both', expand=1)
+    # Correct or Incorrect Message
+    global addCorrectLabel
+    addCorrectLabel = Label(addFrame, text='Enter Your Answer Above')
+    addCorrectLabel.pack(pady=5)
+
+# Create subtract function  ##### this section will need to be removed
+# def subtract():
+#     hideMenuFrames()
+#     subtractFrame.pack(fill='both', expand=1)
 
 # Create multiply function
 def multiply():
@@ -78,7 +162,7 @@ mathMenu.add_command(label='Exit', command=root.quit)
 
 # Create Math Frames
 addFrame = Frame(root, width=400, height=400)
-subtractFrame = Frame(root, width=400, height=400, bg='red')
+subtractFrame = Frame(root, width=400, height=400)
 multiplyFrame = Frame(root, width=400, height=400, bg='yellow')
 divideFrame = Frame(root, width=400, height=400, bg='green')
 
